97835550649fb6826dd72117fa3d045614dd0b6e
Marcos Gardinali <100800469+MarcosGardinali@users.noreply.github.com>
2023-08-08 16:12:07 -0300

    .

--- src/Paginas/Login/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import FormularioLogin from 'Componentes/FormularioLogin'
-// import styles from './Login.module.css'
-
-export default function Login() {
-  return (
-    <FormularioLogin />
-  )
-}
